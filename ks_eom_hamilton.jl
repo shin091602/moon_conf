5bc113d970761c91639246544fd3095ff0d66e4a
shin091602 <shin.soichiro.103@s.kyushu-u.ac.jp>
2026-01-12 13:59:23 +0900

    一旦、まだエラー

--- ks_eom_hamilton.jl
+++ ks_eom_hamilton.jl
@@ -14,37 +14,64 @@ function ks_eom_hamilton(du, u, p, t)
     # パラメータの展開
     mu = p[1]
     C = p[2]
-    Au = [u[1] -u[2] -u[3] u[4];
-          u[2]  u[1] -u[4] -u[3];
-          u[3]  u[4]  u[1]  u[2]]
+    Au = hcat(
+        [u[1], u[2], u[3]],
+        [-u[2], u[1], u[4]],
+        [-u[3], -u[4], u[1]],
+        [u[4], -u[3], u[2]],
+    )
     AuT = transpose(Au)
     
-    X = Au * [u[1]; u[2]; u[3]; u[4]]
+    X = Au * vcat(u[1], u[2], u[3], u[4])
     r_M = sqrt(X[1]^2 + X[2]^2 + X[3]^2)
     r_E = sqrt((X[1] + 1)^2 + X[2]^2 + X[3]^2)
-    p = (1/r_M) * Au * [u[5]; u[6]; u[7]; u[8]]
+    p = (1/r_M) * Au * vcat(u[5], u[6], u[7], u[8])
 
-    dK_U = 0.5 * AuT * [p[1] + X[2];
-                        p[2] - X[1] -1 + mu;
-                        p[3]]
+    dK_U = 0.5 * AuT * vcat(
+        p[1] + X[2],
+        p[2] - X[1] - 1 + mu,
+        p[3],
+    )
     
    H = 0.5*(dot(p,p)) + p[1]*X[2] - p[2]*(X[1] + 1 - mu) - (1 - mu)/r_E - mu/r_M
 
-   dH_X = [-p[2] + (1 - mu)*(X[1] + 1)/r_E^3 + mu*X[1]/r_M^3;
-            p[1] + (1 - mu)*X[2]/r_E^3 + mu*X[2]/r_M^3;
-            (1 - mu)*X[3]/r_E^3 + mu*X[3]/r_M^3]
+   dH_X = vcat(
+        -p[2] + (1 - mu)*(X[1] + 1)/r_E^3 + mu*X[1]/r_M^3,
+        p[1] + (1 - mu)*X[2]/r_E^3 + mu*X[2]/r_M^3,
+        (1 - mu)*X[3]/r_E^3 + mu*X[3]/r_M^3,
+    )
     
-    dX_u = [2u[1] -2u[2] -2u[3] 2u[4];
-            2u[2] 2u[1] -2u[4] -2u[3];
-            2u[3] -2u[4] 2u[1] -2u[2]]
+    dX_u = hcat(
+        [2*u[1], 2*u[2], 2*u[3]],
+        [-2*u[2], 2*u[1], -2*u[4]],
+        [-2*u[3], -2*u[4], 2*u[1]],
+        [2*u[4], -2*u[3], -2*u[2]],
+    )
 
-    rM_u = transpose(dX_u) * [X[1]/r_M; X[2]/r_M; X[3]/r_M]
-    dAuU_u = [u[5] -u[6] -u[7] u[8];
-             u[6]  u[5] -u[8] -u[7];
-             u[7]  u[8]  u[5]  u[6]]
-    dp_u = (-1/(2*r_M^2)) * rM_u * Au * [u[5]; u[6]; u[7]; u[8]] + (1/(2*r_M)) * dAuU_u 
+    # rM_u = transpose(dX_u) * vcat(X[1]/r_M, X[2]/r_M, X[3]/r_M)
+    dAuU_u = hcat(
+        [u[5], u[6], u[7]],
+        [-u[6], u[5], u[8]],
+        [-u[7], -u[8], u[5]],
+        [u[8], -u[7], u[6]],
+    )
 
-    dK_u = 2 * sqrt(r_M) * (H + C/2) + r_M * (transpose(dX_u) * dH_X + transpose(dp_u) * [p[1]+X[2]; p[2]-X[1]-1+mu; p[3]])
+    # AuU = Au * vcat(u[5], u[6], u[7], u[8])
+    # ut = vcat(u[1], u[2], u[3], u[4])
+    # dp_u = (-1/(r_M^2)) * AuU * ut + (1/(2*r_M)) * dAuU_u 
+
+    U   = u[5:8]          # 共役運動量
+    q   = u[1:4]          # KS 位置
+    AuU = Au * U          # 3×1 = A(u)U
+
+    dp_u = (-1/r_M^2) * (AuU * q') + (1/(2*r_M)) * dAuU_u
+#                          ↑ ここで transpose して 1×4 にする
+
+
+    dK_u = 2 * sqrt(r_M) * (H + C/2) + r_M * (
+        transpose(dX_u) * dH_X +
+        transpose(dp_u) * vcat(p[1] + X[2], p[2] - X[1] - 1 + mu, p[3])
+    )
 
     du[1] = dK_U[1]
     du[2] = dK_U[2]
