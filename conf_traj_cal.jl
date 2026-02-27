89b480f2f6ec26cb70bcbb87014bea61da0d8edf
shin091602 <shin.soichiro.103@s.kyushu-u.ac.jp>
2026-02-27 17:41:21 +0900

    check

--- conf_traj_cal.jl
+++ conf_traj_cal.jl
@@ -2,17 +2,80 @@ using Distributed
 using LinearAlgebra
 using DifferentialEquations
 using Plots
+
 include("ks_eom_hamilton.jl")
 
+# Parameters
 num_trj = 10
 mu = 0.01215058426994 # earth-moon system
-C = 3.12 # Jacobi constant
-t_fin = 2.0pi * 100
-u0_temp = [0.0, 0.8, 0.0, 0.0,
-           sqrt(8mu), 0.0, 0.0, 0.0]
+C = 2.95 # Jacobi constant
+t_fin = 2.0pi 
 tspan = (t_fin, 0.0)
-prob = ODEProblem(ks_eom_hamilton, u0_temp, tspan, (mu, C))
-sol = solve(prob, Vern9(), abstol=1e-12, reltol=1e-12)
 
-plot3d(sol[1, :], sol[2, :], sol[3, :], lw=0.5, label="Trajectory", title="KS Trajectory in Earth-Moon System", xlabel="X", ylabel="Y", zlabel="Z")
+# Generate initial conditions with constraints
+u0_list = Vector{Vector{Float64}}(undef, num_trj)
 
+for i in 1:num_trj
+    theta_1 = rand() * pi
+    theta_2 = rand() * pi
+    theta_3 = rand() * 2.0pi
+
+    U1 = 8*mu * cos(theta_1)
+    U2 = 8*mu * sin(theta_1) * cos(theta_2)
+    U3 = 8*mu * sin(theta_1) * sin(theta_2) * cos(theta_3)
+    U4 = 8*mu * sin(theta_1) * sin(theta_2) * sin(theta_3)
+    
+
+    u0_list[i] = [0.0, 0.0, 0.0, 0.0,
+                  U1, U2, U3, U4]  
+end
+
+# Solve ODEs for all initial conditions
+solutions = Vector{Any}(undef, num_trj)
+solutions_xyz = Vector{Any}(undef, num_trj)
+
+println("Solving ODEs for $num_trj trajectories...")
+for i in 1:num_trj
+    println("  Trajectory $i/$num_trj")
+    prob = ODEProblem{true, DifferentialEquations.SciMLBase.FullSpecialize}(
+        ks_eom_hamilton, u0_list[i], tspan, (mu, C))
+    solutions[i] = solve(prob, Vern9(), abstol=1e-12, reltol=1e-12)
+
+    # Convert KS to Cartesian coordinates for plotting
+    num_points = length(solutions[i].t)
+    xyz_traj = zeros(3, num_points)
+    for j in 1:num_points
+        u = solutions[i][j]
+        q = u[1:4]
+        Au = hcat(
+            [u[1], -u[2], -u[3]],
+            [u[2],  u[1],  u[4]],
+            [u[3], -u[4],  u[1]],
+            [u[4], -u[3],  u[2]],
+        )
+        X = Au * q
+        xyz_traj[:, j] = X .+ [1 - mu; 0.0; 0.0]  # Shift to Earth-centered frame
+    end
+    solutions_xyz[i] = xyz_traj
+end
+println("Done!")
+
+# Plot all trajectories on the same interactive figure
+plt = plot3d(
+    solutions_xyz[1][1, :], solutions_xyz[1][2, :], solutions_xyz[1][3, :],
+    lw=0.5, label="Trajectory 1",
+    title="KS Trajectories in Earth-Moon System",
+    xlabel="X", ylabel="Y", zlabel="Z",
+    legend=:outertopright
+)
+
+for i in 2:num_trj
+    plot3d!(plt, solutions_xyz[i][1, :], solutions_xyz[i][2, :], solutions_xyz[i][3, :],
+            lw=0.5, label="Trajectory $i")
+end
+
+xlims!(0.7, 1.3)
+ylims!(-0.3, 0.3)
+zlims!(-0.3, 0.3)
+
+display(plt)
--- ks_eom_hamilton.jl
+++ ks_eom_hamilton.jl
@@ -1,85 +1,114 @@
 using LinearAlgebra
-"""
-u = (u, U)
-Uは正準協約運動量
-(u,U) → (X,p)
-Xは月中心座標系での位置ベクトル
-pはXに対する運動量ベクトル
-mu = 月の質量比
-C = ヤコビ定数
-正準方程式を記述する
-"""
 
-function ks_eom_hamilton(du, u, p, t)
-    # パラメータの展開
-    mu = p[1]
-    C = p[2]
+"""
+u = [q; U] (長さ8ベクトル)
+  q = KS座標 (u1..u4)
+  U = 共役運動量 (U1..U4)
+
+params = [mu, C]
+  mu = 月の質量比
+  C  = Jacobi定数
+
+(X, p_vec) は Moon-centered rotating frame:
+  X = 月中心位置ベクトル (3次元)
+  p_vec = Xに共役な運動量 (3次元)
+
+正則化ハミルトニアン:
+  K(u,U;C) = r_M(u) * (H(X,p) + C/2)
+
+正準方程式:
+  q' = ∂K/∂U
+  U' = -∂K/∂q
+"""
+function ks_eom_hamilton(du, u, params, t)
+    # パラメータ
+    mu = params[1]
+    C  = params[2]
+
+    # KS 位置・運動量
+    q = u[1:4]   # (u1..u4)
+    U = u[5:8]   # (U1..U4)
+
+    # KS 行列 A(u) : 3×4
     Au = hcat(
-        [u[1], u[2], u[3]],
-        [-u[2], u[1], u[4]],
-        [-u[3], -u[4], u[1]],
-        [u[4], -u[3], u[2]],
+        [u[1], -u[2], -u[3]],
+        [u[2],  u[1],  u[4]],
+        [u[3], -u[4],  u[1]],
+        [u[4], -u[3],  u[2]],
     )
-    AuT = transpose(Au)
-    
-    X = Au * vcat(u[1], u[2], u[3], u[4])
+    AuT = transpose(Au)   # 4×3
+
+    # 月中心位置 X = A(u) q
+    X = Au * q
     r_M = sqrt(X[1]^2 + X[2]^2 + X[3]^2)
     r_E = sqrt((X[1] + 1)^2 + X[2]^2 + X[3]^2)
-    p = (1/r_M) * Au * vcat(u[5], u[6], u[7], u[8])
 
-    dK_U = 0.5 * AuT * vcat(
-        p[1] + X[2],
-        p[2] - X[1] - 1 + mu,
-        p[3],
+    # 月中心判定用の閾値
+    epsilon = 1e-10
+
+    if r_M < epsilon
+        # ====================================
+        # 月中心付近の運動方程式
+        du[1] = 0.25 * U[1]
+        du[2] = 0.25 * U[2]
+        du[3] = 0.25 * U[3]
+        du[4] = 0.25 * U[4]
+        du[5:8] .= 0.0
+        return nothing
+    end
+
+    # 運動量 p = (1/(2 r_M)) A(u) U  (正準KS)
+    p_vec = (1/(2*r_M)) * Au * U
+
+    # dK/dU = (1/2) A(u)^T * dH/dp
+    g_p = vcat(
+        p_vec[1] + X[2],
+        p_vec[2] - X[1] - 1 + mu,
+        p_vec[3],
     )
-    
-   H = 0.5*(dot(p,p)) + p[1]*X[2] - p[2]*(X[1] + 1 - mu) - (1 - mu)/r_E - mu/r_M
+    dK_U = 0.5 * AuT * g_p   # 4×1
 
-   dH_X = vcat(
-        -p[2] + (1 - mu)*(X[1] + 1)/r_E^3 + mu*X[1]/r_M^3,
-        p[1] + (1 - mu)*X[2]/r_E^3 + mu*X[2]/r_M^3,
+    # Hamiltonian H(X,p)
+    H = 0.5 * dot(p_vec, p_vec) +
+        p_vec[1]*X[2] - p_vec[2]*(X[1] + 1 - mu) -
+        (1 - mu)/r_E - mu/r_M
+
+    # dH/dX = 3×1
+    dH_X = vcat(
+        -p_vec[2] + (1 - mu)*(X[1] + 1)/r_E^3 + mu*X[1]/r_M^3,
+         p_vec[1] + (1 - mu)*X[2]/r_E^3 + mu*X[2]/r_M^3,
         (1 - mu)*X[3]/r_E^3 + mu*X[3]/r_M^3,
     )
-    
+
+    # dX/du : 3×4
     dX_u = hcat(
-        [2*u[1], 2*u[2], 2*u[3]],
-        [-2*u[2], 2*u[1], -2*u[4]],
-        [-2*u[3], -2*u[4], 2*u[1]],
+        [2*u[1],  2*u[2], 2*u[3]],
+        [-2*u[2], 2*u[1], 2*u[4]],
+        [-2*u[3], -2*u[4],2*u[1]],
         [2*u[4], -2*u[3], -2*u[2]],
     )
 
-    # rM_u = transpose(dX_u) * vcat(X[1]/r_M, X[2]/r_M, X[3]/r_M)
+    # d(Au*U)/du : 3×4
     dAuU_u = hcat(
-        [u[5], u[6], u[7]],
-        [-u[6], u[5], u[8]],
-        [-u[7], -u[8], u[5]],
-        [u[8], -u[7], u[6]],
+        [U[1],  U[2],  U[3]],
+        [-U[2], U[1],  U[4]],
+        [-U[3], -U[4], U[1]],
+        [U[4], -U[3],  U[2]],
     )
 
-    # AuU = Au * vcat(u[5], u[6], u[7], u[8])
-    # ut = vcat(u[1], u[2], u[3], u[4])
-    # dp_u = (-1/(r_M^2)) * AuU * ut + (1/(2*r_M)) * dAuU_u 
-
-    U   = u[5:8]          # 共役運動量
-    q   = u[1:4]          # KS 位置
-    AuU = Au * U          # 3×1 = A(u)U
-
+    # dp/du : 3×4
+    AuU = Au * U             # 3×1
     dp_u = (-1/r_M^2) * (AuU * q') + (1/(2*r_M)) * dAuU_u
-#                          ↑ ここで transpose して 1×4 にする
 
+    # dK/du = 2u(H+C/2) + r_M (X_u^T dH_X + p_u^T dH_dp)
+    dK_u = 2 .* q .* (H + C/2) .+
+           r_M * (
+               transpose(dX_u) * dH_X +
+               transpose(dp_u) * g_p
+           )
 
-    dK_u = 2 * sqrt(r_M) * (H + C/2) + r_M * (
-        transpose(dX_u) * dH_X +
-        transpose(dp_u) * vcat(p[1] + X[2], p[2] - X[1] - 1 + mu, p[3])
-    )
-
-    du[1] = dK_U[1]
-    du[2] = dK_U[2]
-    du[3] = dK_U[3]
-    du[4] = dK_U[4]
-    du[5] = -dK_u[1]
-    du[6] = -dK_u[2]
-    du[7] = -dK_u[3]
-    du[8] = -dK_u[4]
-
+    # 正準方程式
+    du[1:4] .= dK_U
+    du[5:8] .= -dK_u
+    return nothing
 end
